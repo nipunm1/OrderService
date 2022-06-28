8bdf586263294d3351492345441224a7099fbe66
nipunm1 <nipunmiglani.96@gmail.com>
2022-06-28 17:13:35 +0530

    Initial Commit

---
+++ src/main/java/com/cognizant/order/OrderApplication.java
@@ -0,0 +1,21 @@
+package com.cognizant.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class OrderApplication {
+
+	@GetMapping("/order")
+	public String order(){
+		return "order";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
